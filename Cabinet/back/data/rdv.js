bb2d8fe24a51abbcc1a95d01d34a06ee6b9cdcaa
hichembenh <63739246+hichembenh@users.noreply.github.com>
2021-10-15 14:46:52 +0100

    delete a wrong directory

--- Cabinet/back/data/rdv.js
+++
@@ -1,5 +0,0 @@
-const rdvs=[{
-
-}]
-
-export default rdvs
--- Cabinet/front/src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-
-import posts from './posts';
-import auth from './auth';
-import kids from './kids';
-
-export const reducers = combineReducers({ kids,posts,auth });
